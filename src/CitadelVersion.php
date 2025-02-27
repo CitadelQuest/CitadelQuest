24fa1af3699c73dd9ff28afbb23b0749e5ea9741
CitadelQuest <citadelquest@proton.me>
2025-02-27 16:08:24 -0500

    chore(version): Bump version to v0.3.3-alpha

--- src/CitadelVersion.php
+++ src/CitadelVersion.php
@@ -11,7 +11,7 @@ final class CitadelVersion
      * Current version of CitadelQuest
      * @var string
      */
-    public const VERSION = 'v0.3.2-alpha';
+    public const VERSION = 'v0.3.3-alpha';
 
     /**
      * Get the current version string
