8bae7309c5d56a760f8e329b2cb7b2c32e004225
CitadelQuest <citadelquest@proton.me>
2025-05-02 16:11:16 -0400

    fine-tuning: UI/UX, AI Tool Calls

--- src/Service/AnthropicGateway.php
+++ src/Service/AnthropicGateway.php
@@ -317,55 +317,6 @@ class AnthropicGateway implements AiGatewayInterface
      */
     public function getAvailableTools(): array
     {
-        /*
-        curl https://api.anthropic.com/v1/messages \
-            --header "x-api-key: $ANTHROPIC_API_KEY" \
-            --header "anthropic-version: 2023-06-01" \
-            --header "content-type: application/json" \
-            --data \
-            '{
-                "model": "claude-3-7-sonnet-20250219",
-                "max_tokens": 1024,
-                "tools": [{
-                    "name": "get_weather",
-                    "description": "Get the current weather in a given location",
-                    "input_schema": {
-                        "type": "object",
-                        "properties": {
-                            "location": {
-                                "type": "string",
-                                "description": "The city and state, e.g. San Francisco, CA"
-                            },
-                            "unit": {
-                                "type": "string",
-                                "enum": ["celsius", "fahrenheit"],
-                                "description": "The unit of temperature, either 'celsius' or 'fahrenheit'"
-                            }
-                        },
-                        "required": ["location"]
-                    }
-                },
-                {
-                    "name": "get_time",
-                    "description": "Get the current time in a given time zone",
-                    "input_schema": {
-                        "type": "object",
-                        "properties": {
-                            "timezone": {
-                                "type": "string",
-                                "description": "The IANA time zone name, e.g. America/Los_Angeles"
-                            }
-                        },
-                        "required": ["timezone"]
-                    }
-                }],
-                "messages": [{
-                    "role": "user",
-                    "content": "What is the weather like right now in New York? Also what time is it there?"
-                }]
-            }'
-        */
-
         $aiToolCallService = $this->serviceLocator->get(AIToolCallService::class);
         $toolsBase = $aiToolCallService->getToolsDefinitions();
 
