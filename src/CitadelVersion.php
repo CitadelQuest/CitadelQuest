4566dac04b4ca5fe19187554c25a725f918e300e
CitadelQuest <citadelquest@proton.me>
2025-10-12 08:15:41 -0400

    update: - main CitadelQuest version bump-up to: v0.5.6-beta - hide `Settings` from user nav menu

--- src/CitadelVersion.php
+++ src/CitadelVersion.php
@@ -11,7 +11,7 @@ final class CitadelVersion
      * Current version of CitadelQuest
      * @var string
      */
-    public const VERSION = 'v0.5.5-beta'; // 2025-10-11
+    public const VERSION = 'v0.5.6-beta'; // 2025-10-12
 
     /**
      * Get the current version string
