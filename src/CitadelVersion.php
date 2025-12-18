0106c36294b528e0f449bcc15e897647cd72a0ef
CitadelQuest <citadelquest@proton.me>
2025-12-18 13:30:42 +0100

    v0.5.31-beta: Prepare updater for PHP 8.4 migration - vendor clear for versions < 0.5.31

--- src/CitadelVersion.php
+++ src/CitadelVersion.php
@@ -11,7 +11,7 @@ final class CitadelVersion
      * Current version of CitadelQuest
      * @var string
      */
-    public const VERSION = 'v0.5.30-beta'; // 2025-12-13
+    public const VERSION = 'v0.5.31-beta'; // 2025-12-18
 
     /**
      * Get the current version string
