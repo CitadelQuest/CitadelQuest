71ed6299011dae6c771d1885cc1aeff63496dfe9
CitadelQuest <citadelquest@proton.me>
2026-02-13 11:28:07 +0100

    update: main CitadelQuest version bump-up to: 'v0.6.0-beta'

--- src/CitadelVersion.php
+++ src/CitadelVersion.php
@@ -11,7 +11,7 @@ final class CitadelVersion
      * Current version of CitadelQuest
      * @var string
      */
-    public const VERSION = 'v0.5.45-beta'; // 2025-01-20
+    public const VERSION = 'v0.6.0-beta'; // 2026-02-13
 
     /**
      * Get the current version string
