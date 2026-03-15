4a54a943db771ddb8f2db14ea7ab771cd8dff96e
CitadelQuest <citadelquest@proton.me>
2026-03-15 01:37:31 +0100

    Bump version to v0.7.12-beta, fix img-fluid typo in public profile

--- src/CitadelVersion.php
+++ src/CitadelVersion.php
@@ -11,7 +11,7 @@ final class CitadelVersion
      * Current version of CitadelQuest
      * @var string
      */
-    public const VERSION = 'v0.7.11-beta'; // 2026-03-14
+    public const VERSION = 'v0.7.12-beta'; // 2026-03-15
 
     /**
      * Get the current version string
